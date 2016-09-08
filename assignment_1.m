a7355efd085080158264b9c1211161f66f8cb502
Santhosh Nadig <sn@quanox.com>
2016-09-08 22:07:29 +0200

    todays work

--- assignment_1.m
+++ assignment_1.m
@@ -3,10 +3,27 @@
 fxy = @(x,y) 4.*x.*(1-x);
 N = 5;
 [x,y] = meshgrid(linspace(0,1,N), linspace(0,1,N));
-vals = fxy(x,y);
+vals = fxy(x,y)*15;
 disp(vals);
 figure;
-image(vals*255);
-colormap(gray(255));
+image(vals);
+colormap(gray(15));
 
 %%
+% 4.
+u = [1 -3; 4 1];
+v = (1/2)*[1 1; -1 -1];
+w = (1/2)*[1 -1; 1 -1];
+
+uc = u(:);
+nu = (uc'*uc)^(1/2);
+vc = v(:);
+nv = (vc'*vc)^(1/2);
+wc = w(:);
+nw = (wc'*wc)^(1/2);
+disp(['The norm of u is: ',num2str(nu)])
+disp(['The norm of v is: ',num2str(nv)])
+disp(['The norm of w is: ',num2str(nw)])
+disp(['u . v = ', num2str(nu*nv)])
+disp(['u . w = ', num2str(nu*nw)])
+disp(['v . w = ', num2str(nv*nw)])
--- inl1_to_students/ocr_project/matlab/im2segment.m
+++ inl1_to_students/ocr_project/matlab/im2segment.m
@@ -5,28 +5,36 @@ nrofsegments = 4;
 m = size(im,1);
 n = size(im,2);
 
-%
+% Thresholding algorithm. Set all 'pixels' < T = 0
+% This improves contrast.
 T=220;
 im2 = im; im2(im2 < T) = 0;
+% Invert the pixels (convert white -> black & black -> white)
 im2 = not(im2)*255;
 
-% find the start of each character
+% Find the start and end of the entire group
+% start_idx: Start of the characters
+% end_idx  : End of the characters
+% sum of a column of pixels exceeds 'threshold' => separation
 threshold = 255*m*0.965;
 row_sum = sum(im,1);
 start_idx = find(row_sum < threshold, 1);
 end_idx = n - find(fliplr(row_sum) < threshold, 1);
+% row_sum = 1 where characters are present. Otherwise, its zero.
 row_sum = row_sum(start_idx:end_idx) < threshold;
-%figure, plot(row_sum)
+
+% Find each of the individual characters
 segment_start=1;
 for kk = 1:nrofsegments;
     segment_end = segment_start + find(row_sum(segment_start:end) < 1,1) - 1;
-    %fprintf('Segment %d, start = %d, end = %d\n', kk, start_idx+segment_start, start_idx+segment_end);
     temp = zeros(m,n); 
-    temp(:,start_idx +(segment_start:segment_end)) = im2(:,start_idx+(segment_start:segment_end));
+    temp_idx = start_idx +(segment_start:segment_end);
+    temp(:,temp_idx) = im2(:,temp_idx);
     S{kk} = temp;
     segment_start = segment_end + find(row_sum(segment_end:end)>0,1) - 1;
 end;
 segment_end=length(row_sum);
+temp_idx = start_idx +(segment_start:segment_end);
 temp = zeros(m,n);
-temp(:,start_idx +(segment_start:segment_end)) = im2(:,start_idx+(segment_start:segment_end));
+temp(:,temp_idx) = im2(:,temp_idx);
 S{kk+1} = temp;
